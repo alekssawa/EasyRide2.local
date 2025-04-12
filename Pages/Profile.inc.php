9c3e310d87a9f4a935a085b063a34be7b0d7e328
babykiwii <aleks20yara@gmail.com>
2025-04-12 11:55:12 +0300

    12.04.24 Update front profile page v2

--- Pages/Profile.inc.php
+++ Pages/Profile.inc.php
@@ -46,120 +46,22 @@ require_once "../includes/signup.view.inc.php";
             </ul>
         </nav>
     </header>
-    <style>
-        .info-container {
-            display: flex;
-            align-items: center;
-            width: 520px;
-            margin: 20px auto;
-        }
 
-        .info-container2 {
-            display: flex;
-            align-items: center;
-            width: 350px;
-            margin: 20px 845px;
-        }
-
-        .info-box {
-            background-color: #f0f0f0;
-            border-radius: 10px;
-            padding: 20px;
-            flex-grow: 1;
-            margin-left: 20px;
-        }
-
-        .info-box2 {
-            background-color: #f0f0f0;
-            border-radius: 10px;
-            padding: 20px;
-            flex-grow: 1;
-            margin-left: 20px;
-        }
-
-        .info-item {
-            margin-bottom: 10px;
-        }
-
-        .info-label {
-            font-weight: bold;
-        }
-
-        .rounded-image {
-            border-radius: 50%;
-            width: 150px;
-            height: 150px;
-        }
-
-        .info-row {
-            display: flex;
-            align-items: center;
-        }
-
-        .info-detail {
-            display: flex;
-            align-items: center;
-            white-space: nowrap;
-            /* Prevents line breaks */
-        }
-
-        .info-detail p {
-            white-space: normal;
-        }
-
-        .rating-container {
-            margin-left: 20px;
-            white-space: nowrap;
-            /* Prevents line breaks */
-        }
-    </style>
+    <?php
+    if (!isset($_SESSION["user_status"])) {
+        header("Location: ../main.php");
+        exit();
+    }
+    ?>
 
     <!-- TODO: Переделать дизайн -->
     <!-- TODO: Реализовать систему хранение аватарок -->
+
     <div class="info-container">
-        <img src="../img/map2.png" alt="Фото" class="rounded-image">
-        <div class="info-box">
-            <?php if ($_SESSION["user_status"] != "Client") { ?>
-                <div class="info-item">
-                    <span class="info-label">Status:</span> <?php output_status(); ?>
-                </div>
-            <?php } ?>
-            <div class="info-item info-row">
-                <div class="info-detail">
-                    <span class="info-label">ФИО:</span>&nbsp; <?php output_pib(); ?>
-                </div>
-                <?php if ($_SESSION["user_status"] == "Driver") { ?>
-                    <div class="rating-container">
-                        <?php require_once "../includes/Driver_reviews.inc.php"; ?><img src="../img/Star_rating.png" style="width: 20px; height: auto; display: inline-block; vertical-align: middle;" alt="">
-                    </div>
-                <?php } ?>
-            </div>
-            <div class="info-item">
-                <span class="info-label">Телефон:</span> <?php output_phone(); ?>
-            </div>
-            <div class="info-item">
-                <span class="info-label">Email:</span> <?php output_email(); ?>
-            </div>
-        </div>
+        <?php require_once "../includes/Profile_contr.inc.php"; ?>
+        <!-- <?php print_r($order) ?> -->
+
     </div>
-    <!-- <?php if ($_SESSION["user_status"] == "Driver") { ?>
-        <div class="info-container2">
-            <div class="info-box2">
-                <div class="info-item info-row">
-                    <div class="info-detail2">
-                        <?php require_once "../includes/Driver_car.inc.php"; ?>
-                    </div>
-                </div>
-            </div>
-        <?php } ?> -->
-
-        <?php require_once "../includes/Driver_car.inc.php"; ?>
-
-        <div class="info-container">
-            <?php require_once "../includes/Profile_contr.inc.php"; ?>
-            <!-- <?php print_r($order) ?> -->
-
-        </div>
 </body>
 
 </html>
--- includes/Profile_contr.inc.php
+++ includes/Profile_contr.inc.php
@@ -34,7 +34,9 @@ try {
                         </div>
                     </div>
                 </div>
-            </div>';
+            </div>
+            
+            ';
         } else {
             // Заказ не найден
             echo '<p>У вас немає подорожі</p>';
@@ -73,27 +75,51 @@ try {
             $driver['average_rating'] = round($driver['average_rating'], 2);
 
             echo '
-            <div class="profile">
-                <div class="profile-details">
-                    <div class="profile-details-driver">
-                        <div class="profile-driver">
-                            <span>' . htmlspecialchars($driver['driver_p_i_b']) . '</span>
-                            <span>Driver
-                                <img src="../img/Star_rating.png" style="width: 10px; height: auto; display: inline-block;" alt="">
-                                ' . htmlspecialchars($driver['average_rating'] ?? '--') . '
-                            </span>
-                        </div>
-                    
-                    </div>
-                    <div class="line-with-text">DETAILS</div>
-                    <div class="car-driver">
-                        <span class="model">' . htmlspecialchars($driver['car_model']?? 'Неизвестно') . '</span>
-                        <span class="plate">' . htmlspecialchars($driver['car_registration_plate'] ?? 'Неизвестно') . '</span>
-                        <span class="plate">' . htmlspecialchars($driver['car_model_year'] ?? 'Неизвестно') . '</span>
-                        <span class="plate">' . htmlspecialchars($driver['tariff_name'] ?? 'Неизвестно') . '</span>
-                    </div>
+            <div class="order">
+                <div><h2>Details Profile</h2></div></br>
+                <div class="order-details">
+                    <div class="order-details-driver">
+                        <div class="profile-avatar">
+                    <img style="width: 150px; height: 150px;" src="../img/avatar_driver.jpg" alt="Аватар водителя">
                 </div>
-            </div>';
+                <div class="profile-info">
+                    <div class="info-row">
+                        <h2>' . htmlspecialchars($driver['driver_p_i_b']) . '</h2>
+                        <div>
+                            <span class="label">Role</span>
+                            <span class="value">Driver ⭐ ' . htmlspecialchars($driver['average_rating'] ?? '--') . '</span>
+                        </div>
+                        <div>
+                            <span class="label">Phone Number</span>
+                            <span class="value">' . htmlspecialchars($driver['driver_phone_number'] ?? 'Не указано') . '</span>
+                        </div>
+                        <div>
+                            <span class="label">Email Address</span>
+                            <span class="value">' . htmlspecialchars($driver['driver_email'] ?? 'Не указано') . '</span>
+                        </div>
+                    </div>
+                        <div class="line-with-text"><span>CAR</span></div>
+                    <div class="info-row">
+                        <div>
+                            <span class="label">Model</span>
+                            <span class="model">' . htmlspecialchars($driver['car_model'] ?? 'Неизвестно') . '</span>
+                        
+                        </div>
+                        
+                        <div>
+                            <span class="label">Registration Plate</span>
+                            <span class="plate">' . htmlspecialchars($driver['car_registration_plate'] ?? 'Неизвестно') . '</span>
+                        </div>
+
+                        <div>
+                            <span class="label">Tariff</span>
+                            <span class="plate">' . htmlspecialchars($driver['tariff_name']) . '</span>
+                        </div>
+                     </div>
+                </div>
+            </div>
+
+            ';
         } else {
             // Заказ не найден
             echo '<p>У вас немає подорожі</p>';
