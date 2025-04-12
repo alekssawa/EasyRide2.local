40aca129884e0b747c834581856679831be49216
babykiwii <aleks20yara@gmail.com>
2025-04-12 14:43:03 +0300

    12.04.25 Start project on Next ts, express ts

--- includes/List_pages.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo '<a href="../Pages/Profile.inc.php">Profile</a>';
-echo '<a href="../Pages/MyOrder.inc.php">MyOrder</a>';
-echo '<a href="../Pages/MyHistory.inc.php">MyHistory</a>';
-echo '<a href="../includes/logout.inc.php">Logout</a>';
-?>
